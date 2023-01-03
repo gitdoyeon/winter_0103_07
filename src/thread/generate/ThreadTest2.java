e5c557a1db51e114c53a2662ed46cef36a7227e4
gitdoyeon <gitpdyeon@gmail.com>
2023-01-03 14:41:14 +0900

    init commit

--- src/thread/generate/ThreadTest2.java
+++ src/thread/generate/ThreadTest2.java
@@ -1,27 +1,23 @@
 package thread.generate;
 
-import ;
-
-public class ThreadTest2 implements Runnable{
+public class ThreadTest2 implements Runnable {
 
 	@Override
 	public void run() {
-		for(int i=0; i<100; i++) {
-			System.out.println("Thread"+(i+1));
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Runnable을 사용한 Thread"+(i+1));
 			try {
-				Thread.sleep(500);
-			}catch(InterruptedException e) {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
 				e.printStackTrace();
-				
 			}
 		}
-		
 	}
-	
+
 	public static void main(String[] args) {
-		ThreadTest2 t1 = new ThreadTest2();
-		Thread t1 = new Thread(t2);
-		t.start();
+		ThreadTest2 t2 = new ThreadTest2();
+		Thread t = new Thread(t2);//new born 상태
+		t.start();//runnable 상태
 	}
 
 }
